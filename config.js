2c9c2e48a33bb1754550f707cc05aea323f10f49
William Jamison <william.jamison@protonmail.com>
2018-01-12 19:12:35 -0500

    push

---
+++ config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  brew: [
+    'coreutils',
+    'curl --with-openssl',
+    'dnscrypt-proxy',
+    'ffmpeg --with-rtmpdump --with-openssl --with-libass --with-libbs2b --with-rubberband',
+    'ffms2',
+    'git',
+    'gnu-sed --with-default-names',
+    'libass --without-harfbuzz',
+    'mas',
+    'mackup',
+    'mpv --with-vapoursynth --with-libarchive --with-bundle',
+    'mvtools'
+    'nmap',
+    'python',
+    'reattach-to-user-namespace',
+    'sumbliminal',
+    'the_silver_searcher',
+    'tmux',
+    'tree',
+    'vim --with-override-system-vi',
+  ],
+  cask: [
+    'alfred',
+    'arq',
+    'blockblock',
+    'evernote',
+    'firefox',
+    'iterm2',
+    'jitouch'
+    'qlcolorcode',
+    'qlmarkdown',
+    'vagrant',
+    'vmware-fusion'
+    'xquartz'
+  ],
+  gem: [
+    'git-up'
+  ],
+  npm: [
+    'antic',
+    'buzzphrase',
+    'eslint',
+    'instant-markdown-d',
+    // 'generator-dockerize',
+    'gulp',
+    'npm-check',
+    'prettyjson',
+    'trash',
+    'vtop'
+    // ,'yo'
+  ]
+};
